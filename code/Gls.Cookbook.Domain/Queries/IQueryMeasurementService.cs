ee3ed34abcf5f99ee4716dd96ed191b1193af3b7
John Zumbrum <jzumbrum@westfaliausa.com>
2022-09-08 22:43:38 -0400

    Create query measurement service

---
+++ code/Gls.Cookbook.Domain/Queries/IQueryMeasurementService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gls.Cookbook.Domain.Models;
+
+namespace Gls.Cookbook.Domain.Queries
+{
+    public interface IQueryMeasurementService
+    {
+        Task<List<Measurement>> GetAllAsync();
+    }
+}
---
+++ code/Gls.Cookbook.Logic/Measurements/QueryMeasurementService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gls.Cookbook.Domain;
+using Gls.Cookbook.Domain.Models;
+using Gls.Cookbook.Domain.Queries;
+using Gls.Cookbook.Domain.Repositories;
+
+namespace Gls.Cookbook.Logic.Measurements
+{
+    public class QueryMeasurementService : IQueryMeasurementService
+    {
+        private ICookbookContextFactory cookbookContextFactory;
+
+        public QueryMeasurementService(ICookbookContextFactory cookbookContextFactory)
+        {
+            this.cookbookContextFactory = cookbookContextFactory;
+        }
+
+        public async Task<List<Measurement>> GetAllAsync()
+        {
+            await using (ICookbookContext cookbookContext = await cookbookContextFactory.CreateAsync())
+            {
+                return await cookbookContext.MeasurementRepository.GetAllAsync();
+            }
+        }
+    }
+}
