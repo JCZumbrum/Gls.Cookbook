b28cc508a7ff8ed21af8e16e8fb562d666871a79
John Zumbrum <jzumbrum@westfaliausa.com>
2022-09-08 21:43:18 -0400

    Register ingredient services

--- code/Gls.Cookbook.App/MauiProgram.cs
+++ code/Gls.Cookbook.App/MauiProgram.cs
@@ -1,9 +1,10 @@
 ﻿using Gls.Cookbook.DataAccess;
 using Gls.Cookbook.Domain;
+using Gls.Cookbook.Domain.Commands.Ingredients;
 using Gls.Cookbook.Domain.Commands.Measurements;
 using Gls.Cookbook.Domain.Repositories;
+using Gls.Cookbook.Logic.Ingredients;
 using Gls.Cookbook.Logic.Measurements;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Gls.Cookbook.App
 {
@@ -21,9 +22,14 @@ namespace Gls.Cookbook.App
                 });
 
             builder.Services.AddSingleton<ICookbookContextFactory, EfCookbookContextFactory>();
+
             builder.Services.AddTransient<ICommandService<CreateMeasurementCommand>, CreateMeasurementService>();
             builder.Services.AddTransient<ICommandService<UpdateMeasurementCommand>, UpdateMeasurementService>();
             builder.Services.AddTransient<ICommandService<DeleteMeasurementCommand>, DeleteMeasurementService>();
+
+            builder.Services.AddTransient<ICommandService<CreateIngredientCommand>, CreateIngredientService>();
+            builder.Services.AddTransient<ICommandService<UpdateIngredientCommand>, UpdateIngredientService>();
+            builder.Services.AddTransient<ICommandService<DeleteIngredientCommand>, DeleteIngredientService>();
 
             return builder.Build();
         }
