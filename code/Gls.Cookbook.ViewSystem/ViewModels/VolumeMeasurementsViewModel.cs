3950f762b44ab716d92d7e0e5450801a7540ccc8
John Zumbrum <jzumbrum@westfaliausa.com>
2022-09-19 20:25:15 -0400

    Give measurements their own observable

--- code/Gls.Cookbook.ViewSystem/ViewModels/VolumeMeasurementsViewModel.cs
+++ code/Gls.Cookbook.ViewSystem/ViewModels/VolumeMeasurementsViewModel.cs
@@ -13,6 +13,37 @@ namespace Gls.Cookbook.ViewSystem.ViewModels
 {
     public class VolumeMeasurementsViewModel : ObservableObject, IViewModel<EmptyArgs>
     {
+        public class ObservableMeasurement : ObservableObject
+        {
+            public int Id { get; set; }
+
+            private string name;
+            public string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    SetProperty(ref name, value);
+                }
+            }
+
+            private string abbreviation;
+            public string Abbreviation
+            {
+                get
+                {
+                    return abbreviation;
+                }
+                set
+                {
+                    SetProperty(ref abbreviation, value);
+                }
+            }
+        }
+
         private IQueryMeasurementService queryMeasurementService;
 
         public ObservableCollection<ObservableMeasurement> UsMeasurements { get; } = new ObservableCollection<ObservableMeasurement>();
