12c41c3299a3e1d8b208a90f0b310f2361de92b0
John Zumbrum <jzumbrum@westfaliausa.com>
2022-09-27 22:46:46 -0400

    Start building up ingredients viewmodel

--- code/Gls.Cookbook.ViewSystem/ViewModels/IngredientsViewModel.cs
+++ code/Gls.Cookbook.ViewSystem/ViewModels/IngredientsViewModel.cs
@@ -1,16 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Gls.Cookbook.Domain;
+using Gls.Cookbook.Domain.Models;
+using Gls.Cookbook.Domain.Queries;
 using Gls.Cookbook.ViewSystem.Args;
+using static Gls.Cookbook.ViewSystem.ViewModels.MeasurementsViewModel;
 
 namespace Gls.Cookbook.ViewSystem.ViewModels
 {
     public class IngredientsViewModel : ObservableObject, IViewModel<EmptyArgs>
     {
-        public Task InitializeAsync(EmptyArgs args) { return Task.CompletedTask; }
+        private IQueryIngredientService queryIngredientService;
+
+        public class ObservableIngredient : ObservableObject
+        {
+            public int Id { get; set; }
+
+            private string name;
+            public string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    SetProperty(ref name, value);
+                }
+            }
+        }
+
+        public ObservableCollection<ObservableIngredient> Ingredients { get; } = new ObservableCollection<ObservableIngredient>();
+
+        public IngredientsViewModel(IQueryIngredientService queryIngredientService)
+        {
+            this.queryIngredientService = queryIngredientService;
+        }
+
+        public async Task InitializeAsync(EmptyArgs args)
+        {
+            List<Ingredient> ingredients = await queryIngredientService.GetAllAsync();
+
+            Ingredients.AddRange(ingredients.OrderBy(m => m.Name).Select(m => new ObservableIngredient() { Id = m.Id, Name = m.Name }));
+        }
     }
 }
