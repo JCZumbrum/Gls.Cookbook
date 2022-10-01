71a9a17169542035ea2a8a9bccf182969ebc0357
John Zumbrum <jzumbrum@westfaliausa.com>
2022-09-30 23:45:25 -0400

    cleanup using

--- code/Gls.Cookbook.ViewSystem/ViewModels/MeasurementsViewModel.cs
+++ code/Gls.Cookbook.ViewSystem/ViewModels/MeasurementsViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Drawing;
 using System.Linq;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
